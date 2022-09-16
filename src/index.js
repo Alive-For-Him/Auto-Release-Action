3b3fcf3319cae19304c2a7f9da64f27d4ea24702
Jimmy Metz <35279188+jrm402@users.noreply.github.com>
2022-09-16 12:11:09 -0600

    Initial comit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import * as core from '@actions/core';
 import { context } from '@actions/github';
 import GitHub from './github.js';
 
+let gh;
+
 export const run = async () => {
 	const token = process.env.GITHUB_TOKEN;
+	const title = core.getInput('title') ?? 'v$semver';
+	const tag = core.getInput('tag') ?? 'v$semver';
+	const draft = core.getInput('draft') ?? 'false';
+	const changelog = core.getInput('changelog') ?? 'CHANGELOG.md';
+	const changelogEntry = core.getInput('changelog-entry') ?? 'v$semver';
+
 	if (token === undefined) {
 		throw 'The GITHUB_TOKEN environment variable was not set';
 	}
 
-	const gh = new GitHub(token);
+	// init
+	gh = new GitHub(token);
 
+	// get commit info
 	const commits = context.payload.commits;
 	if (commits.length === 0) {
 		core.info('No commits found');
@@ -17,17 +27,16 @@ export const run = async () => {
 
 		return;
 	}
+	const commit = commits[commits.length - 1];
+	const pkgInfo = await getCommitInfo('package.json', commit);
+	const clInfo = await getCommitInfo(changelog, commit);
 
-	console.log('Commit: ', commits);
+	console.log('package.json', pkgInfo);
+	console.log('CHANGELOG.md', clInfo);
 
-	const title = core.getInput('title');
-	const tag = core.getInput('tag');
-	const draft = core.getInput('draft');
-	const changelog = core.getInput('changelog');
-	const changelogEntry = core.getInput('changelog-entry');
-
-	console.log(process.env);
-	console.log(title, tag, draft, changelog, changelogEntry);
+	//
+	// console.log(process.env);
+	// console.log(title, tag, draft, changelog, changelogEntry);
 
 	core.setOutput('released', true);
 	core.setOutput('id', '');
@@ -36,7 +45,44 @@ export const run = async () => {
 	core.setOutput('upload_url', '');
 };
 
-const getChangelog = async (filePath) => {};
+/**
+ * Load a file from a commit
+ */
+const getCommitInfo = async (path, ref) => {
+	let response;
+
+	try {
+		response = await gh.repos.getContent({
+			...context.repo,
+			path,
+			ref,
+		});
+	} catch (e) {
+		if (!e instanceof RequestError) throw e;
+		throw `The path ${path} does not exist`;
+	}
+
+	const { data } = response;
+
+	if (Array.isArray(data)) {
+		throw `The path ${path} is a folder. Please provide a file path.`;
+	}
+
+	if (data.type === 'symlink' && !data.content) {
+		return await getCommitInfo(data.target, ref);
+	}
+
+	if (data.type === 'submodule') {
+		throw `The file cannot be inside of a submodule`;
+	}
+
+	if (!data.content) {
+		return '';
+		// throw `Something went wrong when trying to get the file at ${path}`;
+	}
+
+	return data.content; // atob(data.content);
+};
 
 try {
 	run();
